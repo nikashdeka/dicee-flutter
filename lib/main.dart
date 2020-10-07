447c4f4e1cab24520e8698266e0d209b7f5ca670
nikashdeka <47028816+nikashdeka@users.noreply.github.com>
2020-10-07 16:07:10 +0800

    Dice Game App
    Written in Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,66 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  int counter = 0;
+
+  void rollDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  rollDice();
+                },
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  // rollDice();
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+              ),
+            ),
+          ],
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 50.0),
+          child: RaisedButton.icon(
+            onPressed: () {
+              rollDice();
+            },
+            icon: Icon(
+              Icons.play_circle_filled,
+              size: 32.0,
+            ),
+            label: Text(
+              'ROLL DICE',
+              style: TextStyle(fontSize: 24.0),
+            ),
+            padding: EdgeInsets.all(16.0),
+          ),
+        )
+      ],
+    );
   }
 }
